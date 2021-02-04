01c1c749f41a704897a7fcab90982f6217e9fd1d
Hazel Ip <g00398581@gmit.ie>
2021-02-04 14:54:13 +0000

    week 3 task

---
+++ secondString.py
@@ -0,0 +1,13 @@
+# This program asks a user to input a string and outputs every second letter in reverse order
+# Author: Ka Ling Ip
+
+reverse = input ("Please enter a sentence:") [:: -2]
+print (reverse)
+
+# This part try to print the sentence in reverse order
+reverse = input ("Please enter a sentence:") [:: -1]
+print (reverse)
+
+# [::2] print every second letter 
+reverse = input ("Please enter a sentence:") [:: 2]
+print (reverse)
