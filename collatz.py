b1acc226b25179213ac9cbb206298e2d8e6a6c9e
Hazel Ip <g00398581@gmit.ie>
2021-02-19 06:56:53 +0000

    first try

--- collatz.py
+++ collatz.py
@@ -1,18 +1,18 @@
 # This program ask user to input any positive integer and outputs calculation
 # if the input is even divided by 2, if odd x3 +1
-# end program while output = 0
+# end program while output = 1
 # Author: Ka Ling Ip
 
 # validate input >0
-allNum
-week4 = input("Please enter a positive interger:")
-
+allNum = []
+week4 = int(input("Please enter a positive interger:")) #ask for a positive interger
+# program ends while output =1
 while week4 != 1:
-    if week4%2 == 0:
+    if week4 % 2 == 0:
         week4/2
-        append.
+        allNum.append(week4)
     else:
         week4*3+1
-        append.
+        allNum.append(week4)
 
-print(list)
+print(week4 + allNum)
