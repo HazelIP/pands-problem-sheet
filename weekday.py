5404ed7a46dbb998dbc8ffdc76234263bcb710a5
Hazel Ip <g00398581@gmit.ie>
2021-03-09 05:55:26 +0000

    matching day no. and day of the week, commenting

--- weekday.py
+++ weekday.py
@@ -1,26 +1,15 @@
-# this program out whether or not today is a weekday
+# this program outputs whether or not today is a weekday
 # author: Ka Ling Ip
 
 # use datetime import
-# input of today as "date"
-import datetime #import the built-in module which has a weekday function
-# weekday()
-today = datetime.date(input("enter a date (in 'yyyy.mm.dd' format): "))
-# weekdays as a tuple
-weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-# Find out what day of the week it is
-#day_today = datetime.date(today)
 
-day_today = today.weekday()
-
-day_todayAsString = weekDays[day_today]
-if weekday: #0-4 are monday to friday
-    print ("Yes, unfornately today is a weekday.")
-else: #5,6 are weekend
+import datetime #import the datetime module which has a weekday function
+today = datetime.date.today() #x = datetime.datetime.now() from w3school # namespace of module
+day_today = today.weekday() # Find out what day of the week it is
+today_nice = today.strftime("%A %d. %B %Y") # show day of the week and date
+# print (day_today) #0=monday, 6=sunday
+# print ("Today is {}".format(today_nice))  
+if day_today == 5 or day_today == 6: #5,6 are sunday and saturday respectively  
     print ("It is weekend, yay!")
-
-
-# ask for an input of date in specified format
-# change the input in 0-6 day datetime format (function)
-# if 0-4 print weekday
-# else print weekend 
+else: #0-5 are monday to friday respectively
+    print ("Yes, unfornately today is a weekday.")
