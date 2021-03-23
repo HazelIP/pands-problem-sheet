5b1cd5d7e7d62f4e20bee61f97c435b842618723
Hazel Ip <g00398581@gmit.ie>
2021-03-23 06:19:00 +0000

    made it pretty

--- plottask.py
+++ plottask.py
@@ -1,17 +1,20 @@
-# The program displays a plot of the functions 
+# The program displays a plot of the functions, 
 # f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] on the one set of axes.
-# Some marks will be given for making the plot look nice.
 # author: Ka Ling Ip
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-xpoints = np.array (range(0,4)) #f(x)
-ypoints = xpoints * xpoints #g(x) = x squared
-zpoints = xpoints * xpoints * xpoints # h(x) = x cubed
-
-#plt.plot (xpoints, ypoints, zpoints) # create a plot showing the functions
-plt.plot (xpoints, ypoints, 'b', zpoints, 'y')
-plt.title("Display functions g(x), h(x)")
-plt.legend()
+f = np.array ([0,1,2,3,4]) #f(x) in range [0,4]
+g = f * f #g(x) = f squared
+h = f * f * f # h(x) = f cubed
+ 
+# create a plot showing the 3 functions
+plt.plot (f,'r', linewidth = '3', label = 'f(x)') #f(x) in red line
+plt.plot (g,'b', linewidth = '3', label = 'g(x)') #g(x) in blue line
+plt.plot (h,'y', linewidth = '3', label = 'h(x)') #h(x) in yellow line
+plt.title("Functions f(x), g(x), h(x)")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.legend(loc="upper left")
 plt.show()
