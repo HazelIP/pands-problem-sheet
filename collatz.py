33e93052de229dc3adc593b704081c6c468f0a2b
Hazel Ip <g00398581@gmit.ie>
2021-04-07 05:59:33 +0100

    finished if-else part, ref, comment

--- collatz.py
+++ collatz.py
@@ -4,18 +4,25 @@
 # end program while output = 1
 # Author: Ka Ling Ip
 
-allNum = [] # store the calculations in this list
-week4 = int(input("Please enter a positive interger:")) #ask for a positive interger
-while week4 != 1: # program ends while output =1
-    if week4 % 2 == 0: #do this if user input/successive value is an even number
-        week4/2
-        allNum.append(week4) # put the calculation in the list
-    else:
-        week4*3+1 #do this if user input is an odd number
-        allNum.append(week4) # put the calculation in the list
-print("{} {}".format(week4, allNum))  
+def main(): #create a function called main (ref[4.1])
+    numIn = int(input("Please enter a positive integer:")) #ask for a positive integer
+    allNum = [] #create a list to store the calculations
+    allNum.append(numIn)# store user input in the list (ref[4.2])
+    if numIn > 1: #proceed to calculation only when input is a positive integer (ref[4.3])
+        while numIn != 1: # program ends while output =1 (ref[4.4])
+            if numIn % 2 == 0: #do the following if user input/successive value is an even number
+                numIn = int(numIn/2)
+                allNum.append(numIn) # put the calculation in the list
+            else:
+                numIn = int(numIn*3+1) #do this if user input is an odd number
+                allNum.append(numIn) # put the calculation in the list
+        print (allNum) #print the list of output
+    elif numIn <= 0: #if user input a negative number prompt again
+        main()
 
-# how to validate input >0 (positive)
-#if week4 < 0:
-    #print ("Please enter a POSITIVE interger:")
-#else:
+main() #call the function
+    
+#ref[4.1]: https://www.w3schools.com/python/python_functions.asp 
+#ref[4.2]: https://www.w3schools.com/python/python_lists_add.asp 
+#ref[4.3]: https://www.w3schools.com/python/python_conditions.asp 
+#ref[4.4]: https://www.w3schools.com/python/python_while_loops.asp 
